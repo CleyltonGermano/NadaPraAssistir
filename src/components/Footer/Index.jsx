c9447eaa6431ca6094ecce6087430f59174926e9
CleyltonGermano <cleylton.cg@gmail.com>
2023-02-18 16:39:51 -0300

    correção de bug no arquivo styles do Navbar e adiciona cards

---
+++ src/components/Footer/Index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Styles.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <p>Desenvolvido por: Cleylton Germano</p>
+    </div>
+  );
+}
+
+export default Footer;
--- src/pages/Home/Index.jsx
+++ src/pages/Home/Index.jsx
@@ -1,16 +1,42 @@
 import Navbar from "../../components/Navbar/Index";
+import Footer from "../../components/Footer/Index";
 import React from "react";
-import "./styles.css";
-import MovieCard from "../../components/Carrossel/Index";
+import "./Styles.css";
+import MovieCards from "../../components/Cards/Index";
+import Carousel from "../../components/Carousel/Index";
 
 function Home() {
+
+
+  const items = [
+    {
+      image: "img/childbirth.jpg",
+      imageAlt: "Childbirth education classes",
+      svg: "svg/childbirth_education_classes_icon.webp",
+      svgAlt: "Childbirth education classes",
+      text: "CHILDBIRTH EDUCATION CLASSES",
+      textLink: "https://www.pampers.com/en-us/pregnancy/birthing-classes"
+    }];
+
   return (
     <>
       <Navbar />
-      <div >
-        <h2>Melhores Filmes</h2>
+      <div className="container">
+        <div className="header">
+          <h2>Melhores Filmes</h2>
+        </div>
+        <div className="body">
+          <p>Confira os melhores filmes do momento</p>
+          <div className="carrosel">
+            <Carousel
+              list={<MovieCards />}
+            />
+          </div>
+        </div>
+        <div>
+          <Footer />
+        </div>
       </div>
-      <MovieCard />
     </>
   );
 }
